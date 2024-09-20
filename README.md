8add8902e0c1d21aad9a377a9d1fe6d4f541c70f
Roberto Rodriguez <manufacturedba@gmail.com>
2024-09-19 18:30:04 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FW
+
+This is an art piece with protruding LED noodles. Brightness is controlled by a sliding switch that also responds to ambient sound.
--- code.py
+++ code.py
@@ -1,22 +1,18 @@
-# SPDX-FileCopyrightText: 2022 John Park for Adafruit Industries
-# SPDX-License-Identifier: MIT
-# Motorized fader demo
 import time
 import board
-import analogio
+import pwmio
 
+servo = pwmio.PWMOut(board.A1, frequency=50)
 
-# Slide pot setup
-fader = analogio.AnalogIn(board.A0)
-#fader_position = fader.value  # ranges from 0-65535
-count = 1
-total = fader.value
-# fader_pos = fader.value // 256  # make 0-255 range
-# last_fader_pos = fader_pos
+def servo_duty_cycle(pulse_ms, frequency=50):
+    period_ms = 1.0 / frequency * 1000.0
+    duty_cycle = int(pulse_ms / (period_ms / 65535.0))
+    return duty_cycle
 
 while True:
-    total = total + fader.value
-    count += 1
-    fader_position = total // count
-    print("Fader Position: ", fader_position)
-    time.sleep(1)
+    print("1")
+    servo.duty_cycle = servo_duty_cycle(1.0)
+    time.sleep(1.0)
+    print("0")
+    servo.duty_cycle = servo_duty_cycle(0)
+    time.sleep(1.0)
